74784f53b44d9f52032b51031df27efb68a1a94d
dgreathouse <darryl.greathouse@gmail.com>
2023-11-21 00:19:45 -0500

    first commit

---
+++ src/main/java/frc/robot/lib/Calculator.java
@@ -0,0 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.lib;
+
+/** Add your docs here. */
+public class Calculator {
+
+    /**
+     * A module level variable. All methods in this class have access to this variable
+     */
+    public double m_var = 0.0;
+    /**
+     * 
+     * @param _x The first variable
+     * @param _y The second variable
+     * @return Return the addition of the 2 arguments
+     */
+    public double add(double _x, double _y){
+        return _x + _y;
+    }
+    
+
+
+}
